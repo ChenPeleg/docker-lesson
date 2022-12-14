ce16d994d0b73912668175a470c68c6628e67e24
Alon Wolenitz <alonsukasa@gmail.com>
2022-12-14 23:21:02 +0200

    Added api endpoint

--- lotr/views.py
+++ lotr/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models import Animal
 
 def home(request):
@@ -8,3 +9,9 @@ def home(request):
         'animals': animals,
     }
     return render(request, "index.html", context)
+
+def api(request):
+    animals = Animal.objects.all().values()
+    animals_list = list(animals)
+    
+    return JsonResponse(animals_list, safe=False)
